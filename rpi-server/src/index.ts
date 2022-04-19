4d1fd29d6f2f14febdf5c92bd5f818879fa2b404
dzeju <kublob98@gmail.com>
2022-04-19 15:11:41 +0100

    cleaning

--- rpi-server/src/index.ts
+++ rpi-server/src/index.ts
@@ -1,9 +1,5 @@
 import { initServer } from "./opcua/opcServer";
-import { IMUmodel } from "./models/IMUmodel";
-import { getData } from "./sense-hat/sensors/sensors"
+import { startDataCollection } from "./sense-hat/sensors/sensors"
 
-setInterval(() => {
-  getData()
-}, 500);
-
+startDataCollection()
 initServer();
--- rpi-server/src/sense-hat/sensors/sensors.ts
+++ rpi-server/src/sense-hat/sensors/sensors.ts
@@ -12,22 +12,12 @@ const printData = (err: Event, data: IMUmodel) => {
     console.log(err);
     return;
   }
-  // console.log(data)
   setSensorsData(data);
-  // let str = print_vector3('Accel', data.accel)
-  // str += print_vector3('Gyro', data.gyro)
-  // str += print_vector3('Compass', data.compass)
-  // str += print_vector3('Fusion', data.fusionPose)
-  // str += format('TiltHeading: %s ', headingToDegree(headingCorrection(data.tiltHeading, Math.PI / 2)).toFixed(0));
-
-  // let str2 = "";
-  // if (data.temperature && data.pressure && data.humidity) {
-  //   str2 = format('Temp: %s Pressure: %s Humidity: %s', data.temperature.toFixed(2), data.pressure.toFixed(2), data.humidity.toFixed(2));
-  // }
-  // console.log(str + str2);
-
-  // setTimeout(() => IMU.getValue(printData), 1000);
 }
 
-export const getData = () => { IMU.getValue(printData) };
+export const startDataCollection = () => {
+  setInterval(() => {
+    IMU.getValue(printData)
+  }, 500);
+}
 
